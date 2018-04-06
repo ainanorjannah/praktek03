a6f99517b8d3c26a1d82ae7533b2d9ade0f7f5de
ainanorjannah <ananorjannah0852@gmail.com>
2018-04-06 20:53:50 +0800

    finis

--- src/praktek03/Aksi.java
+++ src/praktek03/Aksi.java
@@ -7,6 +7,7 @@ public class Aksi {
         e.setJenis_padi("unggul");
         e.setHarga(30000);
         e.setMerk("unus");
+       //gak bisa juga pak hee e.setJenisketan();
         
         e.cetakInfo();
         System.out.println("Jenisnnya \t: ");
@@ -15,6 +16,16 @@ public class Aksi {
         System.out.println(e.getHarga());
         System.out.println("merk \t: ");
         System.out.println(e.getMerk());
+        
+        ketan sm = new ketan ();
+        sm.setJenis_padi("inpor");
+        sm.setMerk("unus");
+        sm.setHarga(300000);
+        sm.setJenisketan("lakatan");
+        sm.cetakInfo();
+        
+        System.out.println("Jenis \t: "+sm.getJenisketan());
+        sm.iniketan();
     }
     
 }
