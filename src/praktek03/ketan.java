66b074df68e82ba300b162944d57fd6c959771ad
ainanorjannah <ananorjannah0852@gmail.com>
2018-04-06 20:34:14 +0800

    getter and setter

--- src/praktek03/ketan.java
+++ src/praktek03/ketan.java
@@ -4,7 +4,16 @@ public class ketan extends beras {
     private String jenisketan;
     
     void iniketan (){
-        System.out.println("ulun handak nukar beras gak punya uang"); 
+        System.out.println("ulun handak nukar beras gak punya uang");
+        
+    }
+
+    public String getJenisketan() {
+        return jenisketan;
+    }
+
+    public void setJenisketan(String jenisketan) {
+        this.jenisketan = jenisketan;
     }
     
 }
