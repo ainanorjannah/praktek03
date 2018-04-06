6349fcd5952f668435e1253581754000f714cbaf
ainanorjannah <ananorjannah0852@gmail.com>
2018-04-06 19:17:18 +0800

    Initial commit

---
+++ src/praktek03/Praktek03.java
@@ -0,0 +1,17 @@
+
+package praktek03;
+
+/**
+ *
+ * @author PC
+ */
+public class Praktek03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
