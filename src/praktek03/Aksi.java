59c547561e8f7d1ef4f9598f53623f6fa2945e46
ainanorjannah <ananorjannah0852@gmail.com>
2018-04-06 20:09:40 +0800

    menggunakan method set

--- src/praktek03/Aksi.java
+++ src/praktek03/Aksi.java
@@ -3,9 +3,10 @@ package praktek03;
 public class Aksi {
     public static void main(String[] args) {
         beras e = new beras();
-        e.jenis_padi="lokal";
-        e.harga=500;
-        e.merk="Topi_Koki";
+        
+        e.setJenis_padi("unggul");
+        e.setHarga(30000);
+        e.setMerk("unus");
         
         e.cetakInfo();
     }
