fd817628683a87ab7298e2c56d317a7b11d0d7b3
ainanorjannah <ananorjannah0852@gmail.com>
2018-04-06 20:29:26 +0800

    attribut dan method

--- src/praktek03/ketan.java
+++ src/praktek03/ketan.java
@@ -1,5 +1,10 @@
 package praktek03;
 
-public class ketan {
+public class ketan extends beras {
+    private String jenisketan;
+    
+    void iniketan (){
+        System.out.println("ulun handak nukar beras gak punya uang"); 
+    }
     
 }
