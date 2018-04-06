45f12b60861c74e454f319cb73e18fbfef7ccc7d
ainanorjannah <ananorjannah0852@gmail.com>
2018-04-06 19:58:30 +0800

    nstance objek dan memanggil method

---
+++ src/praktek03/Aksi.java
@@ -0,0 +1,13 @@
+package praktek03;
+
+public class Aksi {
+    public static void main(String[] args) {
+        beras e = new beras();
+        e.jenis_padi="lokal";
+        e.harga=500;
+        e.merk="Topi_Koki";
+        
+        e.cetakInfo();
+    }
+    
+}
